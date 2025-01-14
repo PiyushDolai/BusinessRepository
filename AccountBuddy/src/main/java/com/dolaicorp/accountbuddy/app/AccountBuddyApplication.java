589f6db5ad6140bbeb651fdb2d00a85bc25d78b1
“piyush_dolai” <piyushdolaispd@gmail.com>
2025-01-15 01:55:48 +0530

    First Commit

---
+++ AccountBuddy/src/main/java/com/dolaicorp/accountbuddy/app/AccountBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.dolaicorp.accountbuddy.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountBuddyApplication.class, args);
+	}
+
+}
---
+++ AccountBuddy/src/test/java/com/dolaicorp/accountbuddy/app/AccountBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dolaicorp.accountbuddy.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
